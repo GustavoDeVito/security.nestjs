43a47e0846ccfca694fd6d4f0979e37e4146380b
Gustavo De Vito <gustavodevito38@gmail.com>
2023-03-15 12:16:25 -0300

    feat: add rate limit

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,45 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Req, UseGuards } from '@nestjs/common';
 import { AppService } from './app.service';
+import { SkipThrottle, Throttle } from '@nestjs/throttler';
+import { ThrottlerBehindProxyGuard } from './guards/throttler-behind-proxy.guard';
 
+// @SkipThrottle()
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get('default')
+  default() {
+    return 'Default configuration for Rate limiting and duration.';
+  }
+
+  // Override default configuration for Rate limiting and duration.
+  @Throttle(3, 60)
+  @Get('override')
+  override() {
+    return 'Override default configuration for Rate limiting and duration.';
+  }
+
+  // Rate limiting is applied to this route.
+  @SkipThrottle(false)
+  @Get('dontSkip')
+  dontSkip() {
+    return 'Rate limiting is applied to this route.';
+  }
+
+  // This route will skip rate limiting.
+  @Get('doSkip')
+  doSkip() {
+    return 'This route will skip rate limiting.';
+  }
+
+  // This route get ip
+  @UseGuards(ThrottlerBehindProxyGuard)
+  @Get('proxy')
+  proxy(@Req() req: any) {
+    return {
+      ips: req.ips,
+      ip: req.ip,
+    };
   }
 }
